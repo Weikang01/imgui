e535e0a63aefc4c139e44c30f3cd5f3303623d1e
Pack_Roning <44337630+Weikang01@users.noreply.github.com>
2021-06-10 01:45:25 -0400

    premake5.lua
    add premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,48 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+    staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		staticruntime "On"
+		buildoptions "/MTd"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		staticruntime "On"
+		buildoptions "/MT"
+		optimize "on"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		staticruntime "On"
+		buildoptions "/MT"
+		optimize "on"
