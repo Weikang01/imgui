362f35c9f59f2cd0c43a13ddc46817b55329445e
Pack_Roning <44337630+Weikang01@users.noreply.github.com>
2021-06-11 01:12:56 -0400

    add premake to docking branch
    add premake to docking branch

---
+++ premake5.lua
@@ -0,0 +1,46 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+    staticruntime "On"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"imconfig.h",
+		"imgui.cpp",
+		"imgui.h",
+		"imgui_demo.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_tables.cpp",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		buildoptions "/MTd"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		buildoptions "/MT"
+		optimize "on"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		buildoptions "/MT"
+		optimize "on"
